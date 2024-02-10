49e4cde052326cd9036a70f7f0b86d5de2d66a2e
Iain <iainburge@cmail.carleton.ca>
2024-02-10 16:45:20 -0500

    Mostly working classical implementation of the quantum CORDIC algorithm

--- ClassicalQCORDIC.py
+++ ClassicalQCORDIC.py
@@ -2,7 +2,7 @@
 import numpy as np
 import plotext as plt
 
-from Mult import mult
+from Mult import multImproved as mult
 
 
 def isneg(value: int, n_bits: int) -> bool:
@@ -14,60 +14,6 @@ def isneg(value: int, n_bits: int) -> bool:
     # return ((value%(n_bits+1)) & (1<<n_bits)) != 0
     return value < 0
 
-def asinCORDIC(
-        t: int, n_bits: int
-    ) -> int:
-    """
-    Unitary version of double rotation CORDIC algorithm (TODO: cite the paper)
-    t (int) [0,1]: input angle written in fixed point notation
-    n_bits (int): number of bits used to describe t
-    """
-    theta_i:    int = 0
-    x_i:        int = (1<<(n_bits))-1
-    y_i:        int = 0
-    aux:        int = 0
-    t_i:        int = t % (1<<(n_bits-1))
-    d:   list[bool] = [t>=(1<<(n_bits-1))]
-
-    for i in range(1, n_bits):
-        d.append(isneg(x_i, n_bits) != (y_i>t_i))
-
-        # print(f"{i=}")
-        # print(f"\t{theta_i=}")
-        # print(f"\t{isneg(x_i, n_bits)=} || {y_i=}, {t_i=} || {d[i]=}")
-        # print(f"\t{x_i=}, {aux=}")
-
-        # for _ in range(2): #Rotate twice
-        #     if d[i]:
-        #         x_i -= (y_i>>i)
-        #         y_i, aux = mult(y_i, aux, n_bits, 2*i)
-        #         y_i += (x_i>>i)
-        #     else:
-        #         y_i -= x_i>>i
-        #         x_i, aux = mult(x_i, aux, n_bits, 2*i)
-        #         x_i += y_i>>i
-
-        
-        for _ in range(2):
-            x_i, y_i = (
-                x_i-(-1 if d[i] else 1)*(y_i>>i), 
-                y_i+(-1 if d[i] else 1)*(x_i>>i)
-            )
-        # if d[i]: #swap
-        #     x_i, y_i = y_i, x_i
-        # for _ in range(2): #Rotate twice
-        #     y_i -= x_i>>i
-        #     x_i, aux = mult(x_i, aux, n_bits, 2*i)
-        #     x_i += y_i>>i
-        # if d[i]: #swap;
-        #     x_i, y_i = y_i, x_i
-
-        #Record rotation
-        theta_i += (-1 if d[i] else 1)*2*np.arctan(1/(2**i))
-        #Update t_i
-        t_i, aux = mult(t_i, aux, n_bits, 2*i)
-
-    return theta_i
 
 def asinCORDICClassical(t, n_bits):
     theta_i:    int = 0
@@ -90,48 +36,67 @@ def asinCORDICClassical(t, n_bits):
 
     return theta_i
 
+
 def qasinCORDIC(t, n_bits):
+    """
+    Unitary version of double rotation CORDIC algorithm (TODO: cite the paper)
+    t (int) [0,1]: input angle written in fixed point notation
+    n_bits (int): number of bits used to describe t
+    """
     theta_i:    int = 0
     x_i:        int = (1<<n_bits)-1
     y_i:        int = 0
     t_i:        int = t
-    aux:        int = 0
+    aux_1:      int = 0
+    aux_2:      int = 0
     d:   list[bool] = [False] #Note, first index not used
 
     for i in range(1, n_bits):
         d.append(not(isneg(x_i, n_bits) != (y_i <= t_i)))
 
+        if d[i]:
+            x_i, y_i = y_i, x_i
         for _ in range(2):
-            x_i, y_i = (
-                x_i - (-1 if d[i] else 1)*(y_i>>i),
-                y_i + (-1 if d[i] else 1)*(x_i>>i),
+            x_i -= y_i>>i
+            y_i, aux_1, aux_2 = mult(
+                y_i, aux_1, aux_2, n_bits, 2*i, modulo=False
             )
+            y_i += x_i>>i
+
+        if d[i]:
+            x_i, y_i = y_i, x_i
 
         theta_i += 2*(-1 if d[i] else 1)*np.arctan(2**(-i))
-        t_i, aux = mult(t_i, aux, n_bits, 2*i, modulo=False)
+        t_i, aux_1, aux_2 = mult(
+            t_i, aux_1, aux_2, n_bits, 2*i, modulo=False
+        )
 
     return theta_i
 
 
-
 def main():
-    t       = 128
-    n_bits  = 16
+    n_bits  = 10
 
+    # t       = -181
     # expected = np.arcsin(t/(2**n_bits))
-    # predicted = asinCORDICClassical(t, n_bits)
+    # predicted = qasinCORDIC(t, n_bits)
     # print(f"{t, n_bits=}\t({t/(2**n_bits)})")
     # print("\tExpected:   ", expected)
     # print("\tPredicted:  ", predicted)
     # print("\tDifference: ", predicted-expected)
 
-    test = np.linspace(-(1<<n_bits), (1<<n_bits), num=256, dtype=np.int32)
+    test = np.linspace(-(1<<n_bits), (1<<n_bits), num=128, dtype=np.int32)
     expected  = np.arcsin(test/(1<<n_bits))
     predicted = np.array([qasinCORDIC(t, n_bits) for t in test])
     plt.plot(test, expected, label="Expected", marker='hd')
     plt.plot(test, predicted, label="Predicted", marker="hd")
     plt.title("CORDIC Approx")
     plt.show()
+
+    print(f"{np.max(np.abs(expected-predicted))   = :.6f}")
+    print(f"{np.argmax(np.abs(expected-predicted))= }")
+    print(f"{np.mean(np.abs(expected-predicted))  = :.6f}")
+    print(f"{np.median(np.abs(expected-predicted))= :.6f}")
 
 if __name__ == '__main__':
     main()
--- Mult.py
+++ Mult.py
@@ -1,10 +1,17 @@
 
 import numpy as np
+from functools import cache
+
+@cache
+def fib(i:int) -> int: 
+    return int(np.round(
+        1/(5**.5) * (((1+5**.5)/2)**(i+1)-((1-5**.5)/2)**(i+1))
+    ))
 
 def mult(
-        x: int, aux: int, n: int, m: int, modulo: bool = True, 
+        x: int, aux: int, n: int, m: int, modulo: bool = False, 
         debug: bool = False,
-    ):
+    ) -> tuple[int, int]:
     """
         x   (int): input value
         aux (int): auxiliary register
@@ -21,6 +28,7 @@ def mult(
 
     if debug:
         print(f"{(x,aux)=}")
+
     for i in range(int(np.ceil(n/m))):
         if debug:
             print(f"{aux} {'-' if i%2==1 else '+'} {x>>(m*i)}")
@@ -30,6 +38,47 @@ def mult(
         x, aux = x%(1<<n), aux%(1<<n)
 
     return x, aux
+
+
+def multImproved(
+        x: int, aux_1: int, aux_2: int, n: int, m: int, 
+        modulo: bool = False, debug: bool = False,
+    ) -> tuple[int, int, int]:
+
+    if debug:
+        print(f"Init: \n\t{(x,aux_1,aux_2)=}")
+   
+    aux_2 += x
+    x     += aux_2>>m
+    aux_1 += x
+    
+    for i in range(int(1+2*np.ceil(np.log(n/m)/np.log((1+5**.5))))):
+        if debug:
+            print(f"{i=} | {x=} | {aux_1=}")
+        if i%2 == 0:
+            x       += ((-1)**(fib(i))) * (aux_1>>(m*fib(i)))
+        else:
+            aux_1   += ((-1)**(fib(i))) * (x>>(m*fib(i)))
+
+    aux_2 -= aux_1
+    
+    for i in range(int(1+2*np.ceil(np.log(n/m)/np.log((1+5**.5))))-1, -1, -1):
+        if debug:
+            print(f"{i=} | {x=} | {aux_1=}")
+        if i%2 == 0:
+            x       -= ((-1)**(fib(i))) * (aux_1>>(m*fib(i)))
+        else:
+            aux_1   -= ((-1)**(fib(i))) * (x>>(m*fib(i)))
+
+    aux_1 -= x
+
+    if modulo:
+        x, aux_2 = x%(1<<n), aux_2%(1<<n)
+    
+    if debug:
+        print(f"Result: \n\t{(x,aux_1,aux_2)=}")
+
+    return x, aux_1, aux_2
 
 
 def testMult(n_bits: int = 8, n_tests: int = 16):
@@ -57,14 +106,48 @@ def testMult(n_bits: int = 8, n_tests: int = 16):
     ]
 
     for i in range(len(inputs)):
-        print(f"{inputs[i]=}\t{expected_outputs[i]=}"
-            +f"\t{outputs[i]=}\t{errors[i]=}")
+        print(f"in={inputs[i]} | expect={expected_outputs[i]}"
+            +f" | out={outputs[i]} | err={errors[i]}")
 
+def testMultImproved(n_bits: int = 8, n_tests: int = 16):
+    ran: int = 2**n_bits
+    err: int = 2**(n_bits//2)
+    mr:  int = n_bits//2
+
+    inputs = [
+        (np.random.randint(ran), np.random.randint(err), 
+         np.random.randint(err), np.random.randint(1,mr))
+        for _ in range(n_tests)
+    ]
+    expected_outputs = [
+        ((x+(x>>m)), aux_1, aux_2)
+        for x, aux_1, aux_2, m in inputs
+    ]
+    outputs = [
+        multImproved(x, aux_1, aux_2, n_bits, m)
+        for x, aux_1, aux_2, m in inputs
+    ]
+    errors = [
+        (outputs[i][0]-expected_outputs[i][0], 
+         outputs[i][1]-expected_outputs[i][1],
+         outputs[i][2]-expected_outputs[i][2])
+        for i in range(len(inputs))
+    ]
+
+    for i in range(len(inputs)):
+        print(f"in={inputs[i]}|expect={expected_outputs[i]}"
+            +f"\n\t|out={outputs[i]}|err={errors[i]}")
 
 def main():
     n_bits:     int = 8
     n_tests:    int = 32
-    testMult(n_bits, n_tests)
+    testMultImproved(n_bits, n_tests)
+
+    # x: int = 55
+    #
+    # print(mult(x, 0, n_bits, 1))
+    # print(multImproved(x, 0, 0, n_bits, m=1))
+
     
 if __name__ == '__main__':
     main()
