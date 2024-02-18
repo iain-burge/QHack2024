ea0da02f0fd3109a1a096d4de7422898880ceee1
Iain <iainburge@cmail.carleton.ca>
2024-02-18 01:38:14 -0500

    Found a solution to a problem with the paper's algorithm.

--- ClassicalQCORDIC.py
+++ ClassicalQCORDIC.py
@@ -14,6 +14,60 @@ def isneg(value: int, n_bits: int, modulo: bool = False) -> bool:
         return (value & (1<<(n_bits-1))) != 0
     return value < 0
 
+def asinCORDICCheating(t:int, n_bits:int, debug:bool=False):
+    theta_i:    float = 0
+    x_i:        float = 2**n_bits
+    y_i:        float = 0
+    t_i:        float = t
+    d:      list[int] = [0]
+
+    supertempTheta = np.arcsin(t/(1<<n_bits))
+    tempX = []; tempY = []; tempT = []; tempTheta = []
+
+    for i in range(1, n_bits+2):
+        tempX.append(x_i); tempY.append(y_i); 
+        tempT.append(t_i); tempTheta.append((2**n_bits)*theta_i)
+        
+
+        # d.append(np.sign(x_i) if y_i <= t_i else -np.sign(x_i))
+        if x_i>=0:
+            d.append(1 if y_i <= t_i else -1)
+        else:
+            d.append(1 if t_i<=0 else -1)
+        # d.append(1 if theta_i <= supertempTheta else -1)
+
+        if debug:
+            print(f"|x{i-1}={x_i:.2f}\t|y{i-1}={y_i:.2f}"
+                  +f"\t|t{i-1}={t_i:.2f}\t|theta{i-1}={theta_i:.2f}"
+                  +f"\t|d{i}={d[i]}"
+                  +f"\t|truD{i}={int(1 if theta_i <= supertempTheta else -1)}"
+            )
+        
+        for _ in range(2):
+        # for _ in range(1): #temp
+            x_i, y_i = (
+                x_i - d[i]*(y_i/(2**i)),
+                y_i + d[i]*(x_i/(2**i)),
+            )
+            # r = np.sqrt(x_i**2 + y_i**2) #temp
+            # x_i /= r; y_i /= r
+
+        theta_i += 2*d[i]*np.arctan(2**(-i))
+        t_i     *= (1+2**(-2*i))
+        # t_i = t_i #temp
+
+    # plt.scatter(tempX, label="x", marker='hd')
+    # plt.scatter(tempY, label="y", marker="hd")
+    # plt.scatter(tempT, label="t", marker="hd")
+    # # plt.scatter([(1<<(n_bits-1)) if s else 0 for s in d], label="d", marker="hd")
+    # print(tempTheta)
+    # # plt.plot([1.4437728 for _ in tempTheta], label="correct", marker="hd")
+    # plt.scatter(tempTheta, label="theta", marker="hd")
+    # plt.title("Classical")
+    # plt.show()
+
+    return theta_i
+
 
 def asinCORDICClassical(t, n_bits):
     theta_i:    int = 0
@@ -96,7 +150,7 @@ def qasinModuloCORDIC(t, n_bits):
     t (int) [0,1]: input angle written in fixed point notation
     n_bits (int): number of bits used to describe t
     """
-    n:          int = n_bits + 2
+    n:          int = n_bits+2
     theta_i:    int = 0
     x_i:        int = (1<<n_bits)-1
     y_i:        int = 0
@@ -106,7 +160,7 @@ def qasinModuloCORDIC(t, n_bits):
     d:   list[bool] = [False] #Note, first index not used
 
     # tempX = []; tempY = []
-    for i in range(1, n_bits):
+    for i in range(1, n):
         # tempX.append(x_i); tempY.append(y_i)
         d.append(not(isneg(x_i, n, modulo=True) != (y_i < t_i)))
 
@@ -136,28 +190,30 @@ def qasinModuloCORDIC(t, n_bits):
     return theta_i
 
 def main():
-    n_bits  = 8
-    t       = 252
+    n_bits  = 12
+    t       = 4079
 
     expected  = np.arcsin(t/(2**n_bits))
-    predicted = qasinModuloCORDIC(t, n_bits)
+    predicted = asinCORDICCheating(t, n_bits, True)
     print(f"{t, n_bits=}\t({t/(2**n_bits)})")
     print("\tExpected:   ", expected)
     print("\tPredicted:  ", predicted)
     print("\tDifference: ", predicted-expected)
 
-    test = np.linspace(0, (1<<n_bits), num=256, dtype=np.int32)
+    test = np.linspace(-(1<<n_bits), (1<<n_bits), num=2048, dtype=np.int32)
     expected = np.arcsin(test/(2**n_bits))
-    predicted = np.array([qasinModuloCORDIC(t, n_bits) for t in test])
+    predicted = np.array([asinCORDICCheating(t, n_bits) for t in test])
+
     plt.scatter(test, expected, label="Expected", marker='hd')
     plt.scatter(test, predicted, label="Predicted", marker="hd")
     plt.title("CORDIC Approx")
     plt.show()
 
-    print(f"{np.max(np.abs(expected-predicted))   = :.6f}")
-    print(f"{np.argmax(np.abs(expected-predicted))= }")
-    print(f"{np.mean(np.abs(expected-predicted))  = :.6f}")
-    print(f"{np.median(np.abs(expected-predicted))= :.6f}")
+    print(f"{np.max(np.abs(expected-predicted))          = :.6f}")
+    print(f"{test[np.argmax(np.abs(expected-predicted))] = }")
+    print(f"{np.mean(np.abs(expected-predicted))         = :.6f}")
+    print(f"{np.median(np.abs(expected-predicted))       = :.6f}")
+    # print(abs(expected-predicted))
 
 if __name__ == '__main__':
     main()
